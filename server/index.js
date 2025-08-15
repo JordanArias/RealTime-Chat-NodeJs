cff3ef111a6b81c3078a2558286544cb693610d1
JordanDev <ariasjordan943@gmail.com>
2025-08-15 16:08:53 -0400

    creamos y configuramos index.js

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import logger from 'morgan'
+
+const port = process.env.PORT ?? 3000;
+
+const app = express();
+
+// Usa morgan como middleware
+app.use(logger('dev')); // 'dev' es un formato de registro predefinido
+
+app.get('/', (req, res) =>{
+    res.send('<h1>Esto es el chat</h1>')
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+    
+})
