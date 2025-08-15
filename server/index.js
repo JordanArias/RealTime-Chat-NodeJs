a5fa6d6c2e9323fa6110d74c48a1ab9009266604
JordanDev <ariasjordan943@gmail.com>
2025-08-15 16:55:57 -0400

    creamos u configuramos el index.html

--- server/index.js
+++ server/index.js
@@ -9,7 +9,8 @@ const app = express();
 app.use(logger('dev')); // 'dev' es un formato de registro predefinido
 
 app.get('/', (req, res) =>{
-    res.send('<h1>Esto es el chat</h1>')
+    // res.send('<h1>Esto es el chat</h1>')
+    res.sendFile(process.cwd() + '/client/index.html')
 })
 
 app.listen(port, () => {
